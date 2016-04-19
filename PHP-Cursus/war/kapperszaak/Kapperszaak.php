44a81430ea9d727bc254f5e42392d94429fe6601
thomkok18 <tkok@roc-dev.com>
2016-04-19 19:57:03 +0200

    update

--- PHP-Cursus/war/kapperszaak/Kapperszaak.php
+++ PHP-Cursus/war/kapperszaak/Kapperszaak.php
@@ -1,11 +1,11 @@
 <?php
-echo "De volgende momenten zijn nog beschikbaar:<ul>"; 
+print("De volgende momenten zijn nog beschikbaar:<ul>"); 
 
 $kappersagenda = array("09:45", "10:30");
 $afspraak = array("09:15", "09:30", "10:00", "10:15");
 
-foreach($kappersagenda as $afspraak => $tijd){  
-echo "<li>{$tijd}</li>";
+foreach($kappersagenda as $afspraak => $tijd) {  
+	echo "<li>{$tijd}</li>";
 }
 
-echo "</ul>";
+print("</ul>");
