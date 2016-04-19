1bcbc96ca80641be5ae8383e30e3982ea9948db7
thomkok18 <tkok@roc-dev.com>
2016-04-19 19:49:28 +0200

    update

--- PHP-Cursus/war/kapperszaak/Kapperszaak.php
+++ PHP-Cursus/war/kapperszaak/Kapperszaak.php
@@ -1,10 +1,11 @@
 <?php
-	$zwemclub["De spartelkuikens"] = "25";
-	$zwemclub["De waterbuffels"] = "32";
-	$zwemclub["Plonsmderin"] = "11";
-	$zwemclub["Bommetje"] = "23";
+echo "De volgende momenten zijn nog beschikbaar:<ul>"; 
 
-	foreach ($zwemclub as $naam => $aantal) {
-		echo $naam. $aantal."<br>";
-	}
-?>
+$kappersagenda = array("09:45", "10:30");
+$afspraak = array("09:15", "09:30", "10:00", "10:15");
+
+foreach($kappersagenda as $afspraak => $tijd){  
+echo "<li>{$tijd}</li>";
+}
+
+echo "</ul>";
