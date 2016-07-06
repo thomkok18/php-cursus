036f11b152f3015d88e5c7449fc58cd6fb84fd42
thomkok18 <tkok@roc-dev.com>
2016-07-06 09:53:45 +0200

    update

---
+++ PHP-Cursus/war/functies/footer.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html>
+	<footer><b>
+		Dit is de footer. Dit staat in footer.php.
+	</footer></b>
+</body>
+</html>
---
+++ PHP-Cursus/war/functies/functies.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<section>
+	<section><b>
+		Dit is de section. Dit staat in functies.php.
+	</section></b>
+</body>
+</html>
+
+<?php
+$game = array("World of Warcraft", "Minecraft", "Grepolis", "Darkorbit", "The Settlers");
+foreach ($game as $naam) {
+	echo $naam;
+	for($i = 0; $i <= 5; $i++){		
+		}
+		echo "<br>";
+	}	
+?>
