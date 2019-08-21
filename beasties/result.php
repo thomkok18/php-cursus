8efd364642fa634c44947afb0eddda51511d218b
Thomkok <thomkok13@hotmail.com>
2019-08-21 23:58:18 +0200

    Update

--- beasties/result.php
+++ beasties/result.php
@@ -6,13 +6,13 @@ if (!isset($_GET["brulaap"]) && !isset($_GET["baviaan"]) && !isset($_GET["orang_
     $messages[] = "Kies een aap.";
 } else {
     if (isset($_GET["brulaap"])) {
-        $messages[] = "<img src='img/Brulaap.jpg' height='200px' width='200px' alt='Brulaap'>";
+        $messages[] = "<img src='img/Brulaap.jpg' style='height:200px;width:200px;' alt='Brulaap'>";
     }
     if (isset($_GET["baviaan"])) {
-        $messages[] = "<img src='img/Baviaan.jpg' height='200px' width='200px' alt='Baviaan'>";
+        $messages[] = "<img src='img/Baviaan.jpg' style='height:200px;width:200px;' alt='Baviaan'>";
     }
     if (isset($_GET["orang_oetan"])) {
-        $messages[] = "<img src='img/Oerang-Oetang.jpg' height='200px' width='200px' alt='Oerang-oetang'>";
+        $messages[] = "<img src='img/Oerang-Oetang.jpg' style='height:200px;width:200px;' alt='Oerang-oetang'>";
     }
 }
 if (!count($messages) == 0) {
